e2aa083b750d57522bd7cdb3b3d557c2ee8959ea
Future <Futurekungkub@gmail.com>
2025-08-28 14:29:55 +0700

    Choices and Login

--- bin/project.dart
+++ bin/project.dart
@@ -1,5 +1,84 @@
-import 'package:project/project.dart' as project;
+import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'dart:convert';
 
-void main(List<String> arguments) {
-  print('Hello world: ${project.calculate()}!');
+int? loggedInUserId;
+
+void main() async {
+  bool loggedIn = await login();
+  if (!loggedIn) {
+    print("Login failed. Exiting.");
+    return;
+  }
+
+  while (true) {
+    print("\n======== Expense Tracking App ========");
+    print("1. All expenses");
+    print("2. Today's expense");
+    print("3. Search expense");
+    print("4. Add new expense");
+    print("5. Delete an expense");
+    print("6. Exit");
+    stdout.write("Choose.. ");
+    String? choice = stdin.readLineSync()?.trim();
+
+    if (choice == '1') {
+      await showExpenses();
+    } else if (choice == '2') {
+      await showTodayExpenses();
+    } else if (choice == '3') {
+      stdout.write("Item to search: ");
+      String? keyword = stdin.readLineSync()?.trim();
+      if (keyword == null || keyword.isEmpty) {
+        print("Invalid keyword.");
+        continue;
+      }
+      final results = await searchExpenses(loggedInUserId!, keyword);
+      if (results.isEmpty) {
+        print("No item containing that searching keyword,");
+      } else {
+        for (var exp in results) {
+          final dt = DateTime.parse(exp['date']);
+          final dtLocal = dt.toLocal();
+          print(
+            "${exp['id']}. ${exp['item']} : ${exp['paid']}฿ : ${dtLocal.toString().substring(0, 19)}",
+          );
+        }
+      }
+    } else if (choice == '4') {
+      await addExpense();
+    } else if (choice == '5') {
+      await deleteExpense();
+    } else if (choice == '6') {
+      print("----- Bye -----");
+      break;
+    } else {
+      print("Invalid choice.");
+    }
+  }
+}
+
+Future<bool> login() async {
+  print("====== Login ======");
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+  if (username == null || password == null) {
+    print("Incomplete input");
+    return false;
+  }
+
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: body);
+  if (response.statusCode == 200) {
+    final result = json.decode(response.body) as Map<String, dynamic>;
+    loggedInUserId = result['userId'];
+    return true;
+  } else {
+    print("Error: Status code ${response.statusCode}");
+    print("Response body: ${response.body}");
+    return false;
+  }
 }
